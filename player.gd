31b47be311a1ba7618a302292ab0c86691824da8
yuu-nn <yuna06122@gmail.com>
2025-09-26 00:28:37 +0900

    캐릭터 달리기 구현

--- player.gd
+++ player.gd
@@ -1,7 +1,7 @@
 extends Node2D   # player 노드에 붙이는 스크립트
 
-@onready var sprite1 := $뛰는거지1
-@onready var sprite2 := $뛰는거지2
+@onready var sprite1 := $뛰는거지
+@onready var sprite2 := $거지
 
 var timer := 0.0
 var frame_time := 0.15  # 0.15초마다 이미지 교체
